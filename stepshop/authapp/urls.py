920edc73d5ff6bff703c0369766243cab081e8f3
Лаптев Савва Владимирович <@itstep.loc>
2022-06-08 14:58:07 +0600

    Finish lesson_06

--- stepshop/authapp/urls.py
+++ stepshop/authapp/urls.py
@@ -5,8 +5,8 @@ from authapp.views import login, logout
 app_name = 'auth'
 
 urlpatterns = [
-    path('login', login, name='login'),
-    path('logout', logout, name='logout')
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout')
 ]
 
 
