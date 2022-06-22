32d3428aa7a7c0c710ed2fc2ae638ef16d0dedd5
Лаптев Савва Владимирович <@itstep.loc>
2022-06-22 16:45:15 +0600

    Finish all Work

--- stepshop/stepshop/settings.py
+++ stepshop/stepshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xgjddw#!ut3r1zz=vhe&u5ctx%m%k8^zr07cd6@#!2v*g1^f3b'
+SECRET_KEY = 'django-insecure-ahsdx2323@#sjd@%*=28934aghkasp%^@#@#ahlxuwq@!#@kduwla@231s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
